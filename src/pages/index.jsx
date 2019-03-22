a24624388e461250a23158a7289ffa2a94091eb7
Mish <mish@Mishs-MBP.lan>
2019-03-22 07:40:15 +0200

    PR review- removes whitespace

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 import BaseLayout from '../components/BaseLayout';
 import Hero from '../components/Hero';
-// import SectionsList from '../components/SectionsList';
 import Footer from '../components/Footer';
-
 
 const Homepage = () => (
   <BaseLayout>
