7834539538abaf51d1394d269411e2982280a642
Mish <mishuct26@gmail.com>
2019-04-16 13:08:51 +0200

    adds Evictions whatsapp-number link

--- src/components/SpeedDial/index.jsx
+++ src/components/SpeedDial/index.jsx
@@ -23,10 +23,10 @@ const actions = [
   {
     icon: <Icon path={mdiWhatsapp} size={1} color='#003a70'/>,
     name: 'Whatsapp',
-    href: 'whatsapp://send?text=https://evictions.org.za/'
+    href: 'whatsapp://send?text=hello&phone=+27839624661'
+    // Linking.openURL('whatsapp://send?text=hello&phone=xxxxxxxxxxxxx')
   },
 ];
-
 class SpeedDials extends React.Component {
   state = {
     open: false,
